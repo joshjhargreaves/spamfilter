4f0188eb312c73b890f40b127a8c5e1fa0934646
Josh Hargreaves <jh1288@my.bristol.ac.uk>
2013-12-04 15:15:04 +0000

    added code to add to hashmap

---
+++ src/Filter.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Filter {
+	HashMap<String, Integer> m_spam = new HashMap<String, Integer>();
+	HashMap<String, Integer> m_ham = new HashMap<String, Integer>();
+	public static void main (String[] args) {
+		String files;
+        Filter filter = new Filter();
+		/*Prints out the arguments. Just for testing*/
+        for (String s: args) {
+            System.out.println(s);
+        }
+        /*The first argument is the training folder*/
+		String path = args[0]; 
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		/*Loops through files in the training directory*/
+		for (File f: listOfFiles) {
+			if (f.isFile()) {
+				files = f.getName();
+				if (files.endsWith(".txt") || files.endsWith(".TXT")) {
+					if(files.startsWith("ham")) {
+						filter.readFromFile(f.toString(), true);
+					} else if (files.startsWith("spam")) {
+						filter.readFromFile(f.toString(), false);
+					}
+				}
+			}
+		}
+    }
+    public void readFromFile(String filename, boolean spamFlag) {
+    	Scanner sc = null;
+    	try {
+    		sc = new Scanner(new File(filename));
+    		//useDelimiter(("?<=\\s\\w{1,10})[^\\w\\s])?\\s|[^\\w\\s]$"));
+    	} catch (FileNotFoundException e) {
+    		System.out.println(e);
+    	}
+    	while (sc.hasNext()) {
+    		String w = sc.next();
+    		System.out.println(w);
+    		if(spamFlag == true) {
+    			if(m_ham.containsKey(w) == true) {
+    				m_ham.put(w, m_ham.get(w) + 1);
+    			} else {
+    				m_ham.put(w, 1);
+    			}
+	    	} else {
+	    		if(m_spam.containsKey(w) == true) {
+    				m_spam.put(w, m_spam.get(w) + 1);
+    			} else {
+    				m_spam.put(w, 1);
+    			}
+	    	}
+    	}
+    	sc.close();
+    }
+}
