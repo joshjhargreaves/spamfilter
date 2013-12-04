456e3fd15e780a0123669c706b7f0ceea152745c
Josh Hargreaves <jh1288@my.bristol.ac.uk>
2013-12-04 11:29:20 +0000

    added hashmaps

--- src/filter.java
+++ src/filter.java
@@ -1,9 +1,12 @@
 import java.io.File;
+import java.util.HashMap;
 
 public class filter {
+	HashMap<String, Integer> spam = new HashMap<String, Integer>();
+	HashMap<String, Integer> ham = new HashMap<String, Integer>();
 	public static void main (String[] args) {
-		/*Prints out the arguments. Just for testing*/
 		String files;
+		/*Prints out the arguments. Just for testing*/
         for (String s: args) {
             System.out.println(s);
         }
@@ -12,16 +15,22 @@ public class filter {
 		File folder = new File(path);
 		File[] listOfFiles = folder.listFiles();
 		/*Loops through files in the training directory*/
-		for (File f: listOfFiles) 
-		{
+		for (File f: listOfFiles) {
 			if (f.isFile()) 
 			{
 				files = f.getName();
-				if (files.endsWith(".txt") || files.endsWith(".TXT"))
-				{
+				if (files.endsWith(".txt") || files.endsWith(".TXT")) {
+					if(files.startsWith("ham")) {
+
+					} else if (files.startsWith("spam")) {
+
+					}
 				  System.out.println(files);
 				}
 			}
 		}
+    }
+    public static void readFromFile(String filename) {
+
     }
 }
